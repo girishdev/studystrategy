2ab6692dfac052b184dcee6731631ccd0523fd20
girishdev <girishkumar.aorg@gmail.com>
2021-10-09 22:30:23 +0530

    Update info for menu

--- info.php
+++ info.php
@@ -1,1 +1,35 @@
 This is the info page and Task Page
+
+1. Subject Logos
+2. Information page
+3. README PAGE
+4. MENUS
+   Program
+   - Php
+   - Python
+   - JavaScript
+
+   Database
+   - Mysql
+   - Postgresql
+   - MongoDB
+
+   Wed Developer
+   - Html
+   - Css
+   - jQuery
+   - Ajax
+   - Bootstrap
+   - Material UI
+
+   Framework
+   - Laravel
+   - Yii2
+   - Wordpress
+   - Django
+   - ReactJs
+   - VueJs
+
+   Operating System
+   - Windows
+   - Ubuntu
