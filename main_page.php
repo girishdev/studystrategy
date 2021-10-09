be94ca45f88089f61edd8fb5e36318164bcd5ce9
HemaHSR <hemavatihsr@gmail.com>
2021-10-09 22:02:44 +0530

    Hello oct 09 testing

--- main_page.php
+++ main_page.php
@@ -24,7 +24,7 @@
 
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Study strategies</title>
+	<title>Study strategies Testing...</title>
 	
 	<link href="css/bootstrap.css" rel="stylesheet" />
 	<link href="css/style.css" rel="stylesheet" type="text/css" />
