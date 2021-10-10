9f57848e1eca94e04f78b738b7227fb59d8f22a1
HemaHSR <hemavatihsr@gmail.com>
2021-10-10 20:23:40 +0530

    added tool menu

--- info.php
+++ info.php
@@ -4,42 +4,67 @@ This is the info page and Task Page
 2. Information page
 3. README PAGE
 4. MENUS
-    Program
-    - Php
-    - Python
-    - JavaScript
+Program
+- Php
+- Python
+- JavaScript
+- C
+- C++
 
-    Database
-    - Mysql
-    - Postgresql
-    - MongoDB
+Database
+- Mysql
+- Postgresql
+- MongoDB
+- MariaDB
+- Elastic Search
+- Redis
 
-    Wed Developer
-    - Html
-    - Css
-    - jQuery
-    - Ajax
-    - Bootstrap
-    - Material UI
+Wed Developer
+- Html
+- Css
+- jQuery
+- Ajax
+- Bootstrap
+- Material UI
 
-    Framework
-    - Laravel
-    - Yii2
-    - Wordpress
-    - Symfony
-    - Codeigniter
-    - Django
-    - ReactJs
-    - VueJs
+Framework
+- Laravel
+- Yii2
+- Wordpress
+- Symfony
+- Codeigniter
+- Django
+- ReactJs
+- VueJs
 
-    Operating System
-    - Windows
-    - Ubuntu
+Operating System
+- Windows
+- Ubuntu
 
-    Personal Development
-    - Job Profile
-    - Imp Links
-    - Imp Noted
-    - Doubts
-    - English
-    - Aptitude
+Tools
+- VS Code
+- Sublime
+- JIRA
+- Jmeter
+- Docker
+- Postman
+- Git
+- Agile Metho
+- AWS
+- Google Cloude
+- Azur
+- Networking
+- Algorithm
+- Data structures
+- Big Data
+
+
+
+
+Personal Development
+- Job Profile
+- Imp Links
+- Imp Noted
+- Doubts
+- English
+- Aptitude
