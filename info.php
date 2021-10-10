c3661578693a5cd1ba01c1cc8ffc31fb398abc47
HemaHSR <hemavatihsr@gmail.com>
2021-10-10 20:13:42 +0530

    Added More Menu Items

--- info.php
+++ info.php
@@ -4,32 +4,42 @@ This is the info page and Task Page
 2. Information page
 3. README PAGE
 4. MENUS
-   Program
-   - Php
-   - Python
-   - JavaScript
+    Program
+    - Php
+    - Python
+    - JavaScript
 
-   Database
-   - Mysql
-   - Postgresql
-   - MongoDB
+    Database
+    - Mysql
+    - Postgresql
+    - MongoDB
 
-   Wed Developer
-   - Html
-   - Css
-   - jQuery
-   - Ajax
-   - Bootstrap
-   - Material UI
+    Wed Developer
+    - Html
+    - Css
+    - jQuery
+    - Ajax
+    - Bootstrap
+    - Material UI
 
-   Framework
-   - Laravel
-   - Yii2
-   - Wordpress
-   - Django
-   - ReactJs
-   - VueJs
+    Framework
+    - Laravel
+    - Yii2
+    - Wordpress
+    - Symfony
+    - Codeigniter
+    - Django
+    - ReactJs
+    - VueJs
 
-   Operating System
-   - Windows
-   - Ubuntu
+    Operating System
+    - Windows
+    - Ubuntu
+
+    Personal Development
+    - Job Profile
+    - Imp Links
+    - Imp Noted
+    - Doubts
+    - English
+    - Aptitude
